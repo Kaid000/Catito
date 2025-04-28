a415cbf0789e79c4384eb48ae0f2d4f32780479a
Kaid000 <eugenevoiteshik@gmail.com>
2025-04-28 15:40:55 +0300

    Add User repository

---
+++ Application/Repositories/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Repositories
+{
+   public class UserRepository
+    {
+        private readonly ApplicationDBContext _context;
+
+        public UserRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        // Get User by Credentials
+        public async Task<User> GetUserByEmailAndPasswordHashAsync(string email, string passwordHash)
+        {
+            return await _context.Users
+                .FirstOrDefaultAsync(user => user.Email == email && user.PasswordHash == passwordHash);
+        }
+    }
+}
--- CatsAPI/Program.cs
+++ CatsAPI/Program.cs
@@ -3,20 +3,16 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
 
-// ��������� ��������� OpenAPI (Swagger)
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
     options.UseNpgsql("Host=localhost;Port=8410;Database=catito_database;Username=catito_user;Password=secret"));
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    // �������� Swagger ������ � ������ ����������
     app.UseSwagger();
     app.UseSwaggerUI();
 }
