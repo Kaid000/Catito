5cc16278f0ab28dd7e71831f31d29447a27513d4
Kaid000 <eugenevoiteshik@gmail.com>
2025-04-28 12:04:53 +0300

    hotfix the main proj file

--- CatsAPI/Program.cs
+++ CatsAPI/Program.cs
@@ -1,17 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+// Добавляем поддержку OpenAPI (Swagger)
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    // Включаем Swagger только в режиме разработки
+    app.UseSwagger();
 }
 
 app.UseHttpsRedirection();
