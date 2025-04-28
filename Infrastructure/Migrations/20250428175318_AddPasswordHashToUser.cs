1e7d2cba70840daf392a9cac923cb83a99476bed
Kaid000 <eugenevoiteshik@gmail.com>
2025-04-28 20:55:21 +0300

    Add pgcrypto

---
+++ Infrastructure/Migrations/20250428175318_AddPasswordHashToUser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPasswordHashToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(2025, 4, 28, 17, 53, 17, 896, DateTimeKind.Utc).AddTicks(1702),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValue: new DateTime(2025, 4, 28, 12, 30, 3, 711, DateTimeKind.Utc).AddTicks(6346));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(2025, 4, 28, 17, 53, 17, 896, DateTimeKind.Utc).AddTicks(667),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValue: new DateTime(2025, 4, 28, 12, 30, 3, 711, DateTimeKind.Utc).AddTicks(5903));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(2025, 4, 28, 12, 30, 3, 711, DateTimeKind.Utc).AddTicks(6346),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValue: new DateTime(2025, 4, 28, 17, 53, 17, 896, DateTimeKind.Utc).AddTicks(1702));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(2025, 4, 28, 12, 30, 3, 711, DateTimeKind.Utc).AddTicks(5903),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValue: new DateTime(2025, 4, 28, 17, 53, 17, 896, DateTimeKind.Utc).AddTicks(667));
+        }
+    }
+}
