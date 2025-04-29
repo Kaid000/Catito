19f51b4c5f729327befec268a3e49148c72d7352
Kaid000 <eugenevoiteshik@gmail.com>
2025-04-29 16:38:41 +0300

    update CI mechanism

--- Application/Configuration/Extentions/RepositoryExtention.cs
+++ Application/Configuration/Extentions/RepositoryExtention.cs
@@ -3,8 +3,6 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Application.Configuration.Extentions
 {
-
-
     public static class RepositoriesExtention
     {
         public static void AddRepositories(this IServiceCollection services)
--- Application/Configuration/Extentions/ServiceExtention.cs
+++ Application/Configuration/Extentions/ServiceExtention.cs
@@ -4,8 +4,6 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Application.Configuration.Extentions
 {
-
-
     public static class ServiceExtention
     {
         public static void AddServices(this IServiceCollection services)
